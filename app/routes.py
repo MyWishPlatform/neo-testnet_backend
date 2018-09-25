927f9d036030aaf88bd94d75b760a4839aa8c7c3
Maksim Strenk <ephdtrg@gmail.com>
2018-09-26 00:29:45 +0300

    captcha verify

--- app/routes.py
+++ app/routes.py
@@ -18,24 +18,34 @@ limiter = Limiter(app, key_func=get_remote_address)
 @limiter.limit("1 per day")
 def request_main():
     if request.method == 'POST':
-        neo_address = request.json['address']
+        captcha_responce = request.json['g-recaptcha-responce']
+        if captcha_verify(captcha_responce):
+            neo_address = request.json['address']
 
-        if dblimits.validate_address(address=neo_address) is False:
+            if dblimits.validate_address(address=neo_address) is False:
+                raise RateLimitExceeded
+
+            # generate payload to RPC
+            payload = {
+                "jsonrpc": "2.0",
+                "method": "sendfaucetassets",
+                "params": [str(neo_address)],
+                "id": "1"
+            }
+
+            # send request and fetch response
+            dumped_pl = json.dumps(payload)
+            asset_request = requests.post('http://127.0.0.1:40332', data=dumped_pl)
+            asset_responce = asset_request.json()["result"]["vout"]
+
+            return str(asset_responce)
+        else:
             raise RateLimitExceeded
-
-        # generate payload to RPC
-        payload = {
-            "jsonrpc": "2.0",
-            "method": "sendfaucetassets",
-            "params": [str(neo_address)],
-            "id": "1"
-        }
-
-        # send request and fetch response
-        dumped_pl = json.dumps(payload)
-        asset_request = requests.post('http://127.0.0.1:40332', data=dumped_pl)
-        asset_responce = asset_request.json()["result"]["vout"][0]
-
-        return str(asset_responce)
     return render_template("form.html")
 
+def captcha_verify(captcha_responce):
+    captcha_secret = "6LdK7HEUAAAAAJkjdSQnsN9SUzDT-bBAapMn97Ve"
+    captcha_payload = {"secret": captcha_secret, "responce": captcha_responce}
+    captcha_request = requests.post('https://www.google.com/recaptcha/api/siteverify', captcha_payload)
+    request_answer = json.loads(captcha_request.text)
+    return request_answer['success']
