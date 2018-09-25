e7c52975cc8e91cd51f1040ce703e4c8dd310020
Maksim Strenk <ephdtrg@gmail.com>
2018-09-25 16:24:41 +0300

    template to main

--- app/routes.py
+++ app/routes.py
@@ -12,32 +12,29 @@ dblimits = db_restrictions.DatabaseRestrictions()
 limiter = Limiter(app, key_func=get_remote_address)
 
 
-@app.route('/')
-def request_form():
-    return render_template("form.html")
-
-
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 # restrict number of requests by IP - 1 request for IP per day
 @limiter.limit("1 per day")
 def request_main():
-    neo_address = request.form['text']
+    if request.method == 'POST':
+        neo_address = request.json['address']
 
-    if dblimits.validate_address(address=neo_address) is False:
-        raise RateLimitExceeded
+        if dblimits.validate_address(address=neo_address) is False:
+            raise RateLimitExceeded
 
-    # generate payload to RPC
-    payload = {
-        "jsonrpc": "2.0",
-        "method": "sendfaucetassets",
-        "params": [str(neo_address)],
-        "id": "1"
-    }
+        # generate payload to RPC
+        payload = {
+            "jsonrpc": "2.0",
+            "method": "sendfaucetassets",
+            "params": [str(neo_address)],
+            "id": "1"
+        }
 
-    # send request and fetch response
-    dumped_pl = json.dumps(payload)
-    asset_request = requests.post('http://127.0.0.1:40332', data=dumped_pl)
-    asset_responce = asset_request.json()["result"]["vout"][0]
+        # send request and fetch response
+        dumped_pl = json.dumps(payload)
+        asset_request = requests.post('http://127.0.0.1:40332', data=dumped_pl)
+        asset_responce = asset_request.json()["result"]["vout"][0]
 
-    return str(asset_responce)
+        return str(asset_responce)
+    return render_template("form.html")
 
