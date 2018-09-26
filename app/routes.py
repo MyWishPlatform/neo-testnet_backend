87e75e6e3464e011e124cb0fb6cc375ebec0e8af
Maksim Strenk <ephdtrg@gmail.com>
2018-09-26 17:58:05 +0300

    fix typo in responce

--- app/routes.py
+++ app/routes.py
@@ -16,10 +16,10 @@ limiter = Limiter(app, key_func=get_remote_address)
 @limiter.limit("1 per day")
 def request_main():
     if request.method == 'POST':
-        responce = request.get_json()
-        captcha_result = captcha_verify(responce['g-recaptcha-responce'])
+        response = request.get_json()
+        captcha_result = captcha_verify(response['g-recaptcha-response'])
         if captcha_result["success"]:
-            neo_address = responce['address']
+            neo_address = response['address']
 
             if dblimits.validate_address(address=neo_address) is False:
                 raise RateLimitExceeded
@@ -35,9 +35,9 @@ def request_main():
             # send request and fetch response
             dumped_pl = json.dumps(cli_payload)
             asset_request = requests.post('http://127.0.0.1:40332', data=dumped_pl)
-            asset_responce = asset_request.json()["result"]["vout"]
+            asset_response = asset_request.json()["result"]["vout"]
 
-            return str(asset_responce)
+            return str(asset_response)
         else:
             return make_response(str(captcha_result))
     return render_template("form.html")
@@ -48,9 +48,9 @@ def limit_handler(error):
     return make_response(json.dumps({'error': True, 'code': error.code, 'msg': str(error)}), 429)
 
 
-def captcha_verify(responce):
+def captcha_verify(response):
     secret = "6LdK7HEUAAAAAJkjdSQnsN9SUzDT-bBAapMn97Ve"
-    payload = {"secret": secret, "responce": responce}
+    payload = {"secret": secret, "response": response}
     captcha_request = requests.post('https://www.google.com/recaptcha/api/siteverify', payload)
     request_answer = captcha_request.json()
     return request_answer
