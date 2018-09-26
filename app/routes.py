e7d1ee260708c4823373ccee4d534f1b32e96e6a
Maksim Strenk <ephdtrg@gmail.com>
2018-09-26 03:06:06 +0300

    error handling via api

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,8 @@
 from app import app, db_restrictions
-from flask import render_template
+from flask import render_template, make_response
 from flask_limiter import Limiter, RateLimitExceeded
 from flask_limiter.util import get_remote_address
 from flask_sqlalchemy import request
-from config import Config
 import requests
 import json
 
@@ -18,15 +17,16 @@ limiter = Limiter(app, key_func=get_remote_address)
 @limiter.limit("1 per day")
 def request_main():
     if request.method == 'POST':
-        captcha_responce = request.json['g-recaptcha-responce']
-        if captcha_verify(captcha_responce):
-            neo_address = request.json['address']
+        responce = request.json
+        captcha_result = captcha_verify(responce['g-recaptcha-responce'])
+        if captcha_result["success"]:
+            neo_address = responce['address']
 
             if dblimits.validate_address(address=neo_address) is False:
                 raise RateLimitExceeded
 
             # generate payload to RPC
-            payload = {
+            cli_payload = {
                 "jsonrpc": "2.0",
                 "method": "sendfaucetassets",
                 "params": [str(neo_address)],
@@ -34,18 +34,24 @@ def request_main():
             }
 
             # send request and fetch response
-            dumped_pl = json.dumps(payload)
+            dumped_pl = json.dumps(cli_payload)
             asset_request = requests.post('http://127.0.0.1:40332', data=dumped_pl)
             asset_responce = asset_request.json()["result"]["vout"]
 
             return str(asset_responce)
         else:
-            raise RateLimitExceeded
+            return make_response(str(captcha_result))
     return render_template("form.html")
 
-def captcha_verify(captcha_responce):
-    captcha_secret = "6LdK7HEUAAAAAJkjdSQnsN9SUzDT-bBAapMn97Ve"
-    captcha_payload = {"secret": captcha_secret, "responce": captcha_responce}
-    captcha_request = requests.post('https://www.google.com/recaptcha/api/siteverify', captcha_payload)
-    request_answer = json.loads(captcha_request.text)
-    return request_answer['success']
+
+@app.errorhandler(429)
+def limit_handler(error):
+    return make_response(json.dumps({'error': True, 'code': error.code, 'msg': str(error)}), 429)
+
+
+def captcha_verify(responce):
+    secret = "6LdK7HEUAAAAAJkjdSQnsN9SUzDT-bBAapMn97Ve"
+    payload = {"secret": secret, "responce": responce}
+    captcha_request = requests.post('https://www.google.com/recaptcha/api/siteverify', payload)
+    request_answer = captcha_request.json()
+    return request_answer
