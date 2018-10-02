a7b494ecdf1e7e8e1fa17a7025afce5f001c8ce0
aleksei-udalov <aleksei.udalov.01@gmail.com>
2018-10-02 13:27:03 +0200

    bot messages

--- bot.py
+++ bot.py
@@ -11,6 +11,15 @@ from app.db_restrictions import DatabaseRestrictions
 
 from settings_local import BOT_TOKEN, FAUCET_CLI
 
+MESSAGE_BAD_ADDR = 'Invalid address. Please check your input.'
+MESSAGE_LIMIT_ADDR = 'You have exceeded the number of requests allowed in one day. Please try again tomorrow.'
+MESSAGE_SUCCESS = 'Assets were successfully sent.'
+MESSAGE_ERROR = 'An internal server error has occurred. Please, try again later.'
+
+DESC = '''The faucet provides 1000 NEO and Gas for one NEO address from one IP address.
+Webpage: https://neo.mywish.io'''
+
+
 ADDRESS_VERSION = 23
 
 def double_hash(data):
@@ -34,34 +43,39 @@ def work(bot):
     global update_id
     for update in bot.get_updates(offset=update_id, timeout=10):
         update_id = update.update_id + 1
-        if update.message:
-            if not check_address(update.message.text):
-                update.message.reply_text('Please enter correct NEO address')
-                continue
-            address = update.message.text
-        
-            asset_request = DatabaseRestrictions.find_address(address)
-            if asset_request is not None and not DatabaseRestrictions.is_enough_time(asset_request.last_request_date):
-                update.message.reply_text('This address was used recently. Please wait before next request')
-                continue
-            telegram_address = DatabaseRestrictions.find_telegram_address(str(update.message.from_user.id))
-            if telegram_address is not None and not DatabaseRestrictions.is_enough_time(telegram_address.last_request_date):
-                update.message.reply_text('This telegram address was used recently')
-                continue
-            try:
-                send_funds(address)
-                if asset_request:
-                    DatabaseRestrictions.update_request(asset_request)
-                else:
-                    DatabaseRestrictions.store_address(DatabaseRestrictions.new_entry(address))
-                if telegram_address:
-                    DatabaseRestrictions.update_request(telegram_address)
-                else:
-                    DatabaseRestrictions.store_address(DatabaseRestrictions.new_telegram_entry(str(update.message.from_user.id)))
-                update.message.reply_text('Your funds sucessfully sent')
-            except:
-                traceback.print_exc()
-                update.message.reply_text('Some error. Try later or contact support')
+        if not update.message:
+            continue
+        if update.message.text in ('/start', '/help'):
+            update.message.reply_text(DESC)
+            continue
+
+        if not check_address(update.message.text):
+            update.message.reply_text(MESSAGE_BAD_ADDR)
+            continue
+        address = update.message.text
+    
+        asset_request = DatabaseRestrictions.find_address(address)
+        if asset_request is not None and not DatabaseRestrictions.is_enough_time(asset_request.last_request_date):
+            update.message.reply_text(MESSAGE_LIMIT_ADDR)
+            continue
+        telegram_address = DatabaseRestrictions.find_telegram_address(str(update.message.from_user.id))
+        if telegram_address is not None and not DatabaseRestrictions.is_enough_time(telegram_address.last_request_date):
+            update.message.reply_text(MESSAGE_LIMIT_ADDR)
+            continue
+        try:
+            send_funds(address)
+            if asset_request:
+                DatabaseRestrictions.update_request(asset_request)
+            else:
+                DatabaseRestrictions.store_address(DatabaseRestrictions.new_entry(address))
+            if telegram_address:
+                DatabaseRestrictions.update_request(telegram_address)
+            else:
+                DatabaseRestrictions.store_address(DatabaseRestrictions.new_telegram_entry(str(update.message.from_user.id)))
+            update.message.reply_text(MESSAGE_SUCCESS)
+        except:
+            traceback.print_exc()
+            update.message.reply_text(MESSAGE_ERROR)
 
 
 
